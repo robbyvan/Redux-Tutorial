e295d3b9bbb66644d5394b42b1f8be615612f4bc
robbyvan <fzpinus@gmail.com>
2017-04-02 15:01:33 -0700

    actions

--- dev/js/containers/UserList.js
+++ dev/js/containers/UserList.js
@@ -4,12 +4,18 @@ import { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import { selectUser } from '../actions/AllActions.js'
+
 export class UserList extends Component {
 
   createListItems() {
     return this.props.users.map((user) => {
       return (
-        <li key={user.id}>{user.first} {user.last}</li>
+        <li key={user.id}
+            onClick={() => this.props.selectUser(user)}
+            >
+          {user.first} {user.last}
+        </li>
       );
     });
   }
@@ -30,4 +36,11 @@ function mapStateToProps (state) {
   };
 }
 
-export default connect(mapStateToProps)(UserList);
+function matchDispatchToProps(dispatch){
+  return bindActionCreators({
+    selectUser: selectUser
+  }, dispatch);
+}
+
+
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
