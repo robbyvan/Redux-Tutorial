ca2d968dbd648fb84b89eb691049696c581dd80f
robbyvan <fzpinus@gmail.com>
2017-04-01 20:58:44 -0700

    dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,48 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    devServer: {
-        inline: true,
-        contentBase: './src',
-        port: 3000
-    },
-    devtool: 'cheap-module-eval-source-map',
-    entry: './dev/js/index.js',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
+  entry: './dev/js/index.js',
     output: {
-        path: 'src',
-        filename: 'js/bundle.min.js'
+        path: 'src/js',
+        filename: 'bundle.min.js'
     },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
+    devServer: {
+      inline: true,
+      contentBase: './src',
+      port: 3000
+    },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: ["babel-loader"],
+        query: {
+          presets: ["latest", "stage-0", "react"]
+        }
+      },
+      {
+        test: /\.json$/,
+        exclude: /(node_modules)/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader'
+      },
     ]
-};
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ]
+}
